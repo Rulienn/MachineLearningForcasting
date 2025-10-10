75bc162d63c502c6d6f9268088f891cbb7777a0f
Rulienn <julien.gayraud7@gmail.com>
2025-10-10 10:27:39 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,3 +44,41 @@ def create_df(path: str) -> pd.DataFrame:
 
     df = pd.concat(lst)
     return df
+
+
+
+# input dataframe : contains all rm_id, net_weight_sum and date (at least these three columns)
+# name of the parameter : df_final
+def print_submission(df_final, filename="submission.csv"):
+
+    df_final["rm_id"] = pd.to_numeric(df_final["rm_id"], errors="coerce").astype("Int64")
+
+    # 3) Load the ID mapping
+    ids = pd.read_csv("./data/prediction_mapping.csv")  # or your actual path
+    ids["forecast_start_date"] = pd.to_datetime(ids["forecast_start_date"])
+    ids["forecast_end_date"]   = pd.to_datetime(ids["forecast_end_date"])
+    ids["rm_id"]    = pd.to_numeric(ids["rm_id"], errors="coerce").astype("Int64")
+    ids = ids.rename(columns={
+        "forecast_end_date": "date"
+    })
+
+    # 4) For each ID, get cum at end_date (<= end_date)
+
+    out = (
+        ids.merge(df_final, on=["rm_id", "date"], how="left")
+        .assign(cum=lambda d: d["cum"].fillna(0))   # cum=0 quand absent
+        .sort_values(["rm_id", "date"])
+        .reset_index(drop=True)
+    )
+
+
+    # 5) Cum at day before start_date
+    submission = (
+        out[["ID", "cum"]]
+        .rename(columns={"cum": "predicted_weight"})   # if your file needs "predicted_weight"
+        .fillna({"predicted_weight": 0})
+        .sort_values("ID")
+    )
+
+    submission.to_csv("./submissions/" + filename, index=False)
+    print(submission.head())
